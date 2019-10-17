1afb644aebc247231cf840d05554ba8f0de5ab23
rs <reissedler@gmail.com>
2019-10-17 18:41:36 +0100

    fixed error

--- search.py
+++ search.py
@@ -42,11 +42,11 @@ for num, word in enumerate(words_list):
     print('{0}. URL : {1}'.format(str(num + 1), url_base + word))
     try:
         driver.get(url_base + word)
-        time.sleep()
+        time.sleep(2)
         print('\t' + driver.find_element_by_tag_name('h2').text)
     except Exception as e1:
             print(e1)
-            time.sleep()
+            time.sleep(2)
 
 driver.close()
 
